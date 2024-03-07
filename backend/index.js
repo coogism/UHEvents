2c12e3af5a2b8a023ba0105655d8d1dd7ff4be32
weabruhnie <howtodrawapicture@gmail.com>
2024-03-07 13:54:01 -0600

    Added .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const { default: axios } = require("axios");
+
+axios({
+    method : "get",
+    baseURL : "https://getinvolved.uh.edu/api/discovery/event/search",
+    params : {
+        "themes[0]" : "Social"
+    }
+}).then(result => {
+    console.log(result.data.value)
+})
