fffe23a89d098ad60bd8066834d19e4a7da737ce
weabruhnie <howtodrawapicture@gmail.com>
2024-05-09 14:26:15 -0500

    updated api frontend

--- frontend/src/elements/api.js
+++ frontend/src/elements/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "https://api.weabonie.com"
-    baseURL: "http://localhost:8080"
+    baseURL: "https://api.weabonie.com"
+    // baseURL: "http://localhost:8080"
 })
